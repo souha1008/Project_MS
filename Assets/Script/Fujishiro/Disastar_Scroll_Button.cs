f17f583d5e4447c6dbd608837277999a337182bf
FUJISHIRO_DESKTOP <fujishiro.haruki.hal@gmail.com>
2024-01-02 14:23:22 +0900

    キャラ進化スペックスプライト変更＆配置

--- Assets/Script/Fujishiro/Disastar_Scroll_Button.cs
+++ Assets/Script/Fujishiro/Disastar_Scroll_Button.cs
@@ -14,6 +14,9 @@ public class Disastar_Scroll_Button : MonoBehaviour
     public class Disastar_Dictionary : SerializableDictionary<string, Sprite, StringSpriteKeyValuePair> { }
 
     [Serializable]
+    public class DisBG_Dic : SerializableDictionary<string, Sprite, StringSpriteKeyValuePair> { }
+
+    [Serializable]
     public class StringDDKeyValuePair : SerializableKeyValuePair<string, Disastar_Dictionary> { }
 
     [Serializable]
@@ -21,8 +24,11 @@ public class Disastar_Scroll_Button : MonoBehaviour
 
     [SerializeField] Image target_icon;
     [SerializeField] Image target_spec;
+    [SerializeField] Image target_BG;
+
     [SerializeField] Disastar_Dictionary disastar_icon;
     public d dic;
+    [SerializeField] DisBG_Dic disBG_dic;
     [SerializeField] GameObject Scroll;
 
     
@@ -35,6 +41,9 @@ public class Disastar_Scroll_Button : MonoBehaviour
         target_icon.sprite = disastar_icon[disaName];
 
         target_spec.sprite = dic[dicName][disaName];
+
+        target_BG.sprite = disBG_dic[disaName];
+
         Scroll.SetActive(false);
     }
     // Ë¶êŒê‡ñæ
