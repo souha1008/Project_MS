1d0abcebf36668e695492894df40ca4647b53d54
FUJISHIRO_DESKTOP <fujishiro.haruki.hal@gmail.com>
2024-01-12 17:34:43 +0900

    各種災害ステート作成。オーバーレイ作成

---
+++ Assets/Script/Fujishiro/Effect/EffectState/EffectState_Hurricane.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class EffectState_Hurricane : BaseEffectState
+{
+
+}
--- Assets/Script/Fujishiro/Effect/EffectState/EffectState_IceAge.cs
+++ Assets/Script/Fujishiro/Effect/EffectState/EffectState_IceAge.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 [CreateAssetMenu]
 public class EffectState_IceAge : BaseEffectState
