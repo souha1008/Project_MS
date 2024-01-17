f423625cae58f0f482751704f268866264f6347e
shika-1012 <shikashika1012@outlook.jp>
2024-01-17 11:35:19 +0900

    敵追加

---
+++ Assets/Script/shikama/Status/OwlStatus.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlStatus : AnimalStatus
+{
+    public OwlStatus(OwlBaseStatus baseStatus, Animal animal) : base(baseStatus, animal)
+    {
+
+    }
+}
---
+++ Assets/Script/shikama/Status/TigerStatus.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TigerStatus : AnimalStatus
+{
+    public TigerStatus(TigerBaseStatus baseStatus, Animal animal) : base(baseStatus, animal)
+    {
+
+    }
+}
